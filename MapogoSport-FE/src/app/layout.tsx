e54ee0c023d908daafa61bae63b255b2c165d210
nguyentuakina <nguyentuakinapy@gmail.com>
2024-09-16 16:03:47 +0700

    ''

--- MapogoSport-FE/src/app/layout.tsx
+++ MapogoSport-FE/src/app/layout.tsx
@@ -3,11 +3,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -28,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistMono.variable}`}>
+      <body className={geistMono.className}>
         {children}
       </body>
     </html>
--- MapogoSport-FE/src/app/page.tsx
+++ MapogoSport-FE/src/app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div> 
+      Hello World!
     </div>
   );
 }
