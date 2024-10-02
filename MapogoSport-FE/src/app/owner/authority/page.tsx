d241257540fc1c306edca74856bc6666bde56526
DieuMy25 <131366686+DieuMy25@users.noreply.github.com>
2024-10-02 14:10:52 +0700

    50% authority

--- MapogoSport-FE/src/app/owner/authority/page.tsx
+++ MapogoSport-FE/src/app/owner/authority/page.tsx
@@ -1,12 +1,67 @@
-function test() {
+import { Table } from "react-bootstrap";
+
+function Test() {
 
     return (
         <>
-
-            <div>
-fbfbvfbfc
+            <h3 className="text-center text-danger fw-bold" style={{ fontSize: '20px' }}>PHÂN QUYỀN NGƯỜI DÙNG</h3>
+            <div className="d-flex align-items-center mb-3">
+                <i className="bi bi-funnel me-2 fs-4"></i>
+                <select className="form-select" style={{ width: "auto" }}>
+                    <option selected>Vai trò</option>
+                    <option value="Khách hàng">Khách hàng</option>
+                    <option value="Chủ sân">Chủ sân</option>
+                    <option value="Nhân viên">Nhân viên</option>
+                    <option value="Quản trị viên">Quản trị viên</option>
+                </select>
+                <div className="ms-auto">
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Tìm kiếm" />
+                        <button className="btn btn-outline-secondary" type="submit">
+                            <i className="bi bi-search"></i>
+                        </button>
+                    </form>
+                </div>
             </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th rowSpan={2}>STT</th>
+                        <th rowSpan={2}>Tài khoản</th>
+                        <th rowSpan={2}>Họ và tên</th>
+                        <th colSpan={4}>Vai trò</th>
+                    </tr>
+                    <tr>
+                        <th>Khách hàng</th>
+                        <th>Chủ sân</th>
+                        <th>Nhân viên</th>
+                        <th>Quản trị viên</th>
+                    </tr>
+                </thead>
+                <tbody data-bs-toggle="modal" data-bs-target="#exampleModalToggle">
+                    <tr>
+                        <td>1</td>
+                        <td>Sân bóng 1</td>
+                        <td>Nguyễn Văn A</td>
+                        <td><input disabled className="form-check-input" type="checkbox" /></td>
+                        <td><input disabled className="form-check-input" type="checkbox" /></td>
+                        <td><input disabled className="form-check-input" type="checkbox" /></td>
+                        <td><input disabled className="form-check-input" type="checkbox" /></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Sân bóng 1</td>
+                        <td>Nguyễn Văn A</td>
+                        <td><input disabled className="form-check-input" type="checkbox" /></td>
+                        <td><input disabled className="form-check-input" type="checkbox" /></td>
+                        <td><input disabled className="form-check-input" type="checkbox" /></td>
+                        <td><input disabled className="form-check-input" type="checkbox" /></td>
+                    </tr>
+                </tbody>
+            </Table>
+
         </>
     )
 }
-export default test;
+
+export default Test;
