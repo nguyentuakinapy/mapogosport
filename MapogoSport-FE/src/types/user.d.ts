57ed17ad2f515926d8543cc6035467d088ff8014
Phi Hung <phihung.devka@gmail.com>
2024-10-07 17:58:35 +0700

    Fix function user profile

--- MapogoSport-FE/src/types/user.d.ts
+++ MapogoSport-FE/src/types/user.d.ts
@@ -3,11 +3,11 @@ interface User {
     fullname: string;
     password: string;
     enabled: number;
-    gender: boolean;
+    gender: boolean | null;
     createdAt: Date;
-    birthday: Date;
+    birthday: Date | null;
     email: string;
-    image: string;
+    image: string | null;
     authorities: [
         {
             authorityId: number;
@@ -17,16 +17,4 @@ interface User {
             }
         }
     ];
-}
-
-interface User1 {
-    username: string;
-    fullname: string;
-    password: string;
-    enabled: number;
-    gender: boolean;
-    createdAt: Date;
-    birthday: Date;
-    email: string;
-    image: string;
 }
