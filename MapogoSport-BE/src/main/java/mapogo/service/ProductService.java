28128a298d895c0be9c0940e1bcc208a08426d68
Nguyễn Tú <164324917+nguyentuakinapy@users.noreply.github.com>
2024-10-08 18:07:19 +0700

    Update ProductService.java

--- MapogoSport-BE/src/main/java/mapogo/service/ProductService.java
+++ MapogoSport-BE/src/main/java/mapogo/service/ProductService.java
@@ -6,10 +6,6 @@ import mapogo.entity.Product;
 
 public interface ProductService {	
 	List<Product> findAll();
-<<<<<<< HEAD
 	
-	Optional<Product> findById(Integer id); 
-=======
 	Optional<Product> findById(Integer id); // Sửa lại tên phương thức
->>>>>>> 695ed675d69c9699b1b514de646972edabe2eda3
 }
