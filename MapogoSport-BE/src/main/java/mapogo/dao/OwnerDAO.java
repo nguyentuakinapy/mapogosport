ad507625ba65c9159656e77070d471ea26276472
DieuMy25 <131366686+DieuMy25@users.noreply.github.com>
2024-10-14 14:54:11 +0700

    My commit SportFieldManager 20

--- MapogoSport-BE/src/main/java/mapogo/dao/OwnerDAO.java
+++ MapogoSport-BE/src/main/java/mapogo/dao/OwnerDAO.java
@@ -3,7 +3,8 @@ package mapogo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mapogo.entity.Owner;
+import mapogo.entity.User;
 
 public interface OwnerDAO extends JpaRepository<Owner, Integer>{
-
+	Owner findByUser(User username);
 }
