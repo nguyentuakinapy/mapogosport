3c11b45b489a025b9eabb7211ba2301e9f59fb04
DieuMy25 <131366686+DieuMy25@users.noreply.github.com>
2024-10-15 21:10:35 +0700

    update SportFiledManager

--- MapogoSport-FE/src/app/owner/sport-manager/page.tsx
+++ MapogoSport-FE/src/app/owner/sport-manager/page.tsx
@@ -9,7 +9,7 @@ import Tabs from 'react-bootstrap/Tabs';
 const SportFieldList = () => {
     const [sportFields, setSportFields] = useState([]);
     const [user, setUser] = useState(null);
-    
+
     useEffect(() => {
         const userSession = sessionStorage.getItem('user');
         const user = userSession ? JSON.parse(userSession) : null;
@@ -40,28 +40,36 @@ const SportFieldList = () => {
                 </div>
             </div>
             <br />
-            <div className='card p-2'>
-                <div className='d-flex justify-content-between'>
-                    <div>
-                        <b className='mx-3'>1</b>
-                        <img src="/images/logo.png" style={{ width: '300px' }} alt="cc" />
-                    </div>
-                    <div className='me-auto mt-3'>
-                        <b >Tên sân</b><br />
-                        <b className='font-14'>652 Đ. Tô Ký, Tân Chánh Hiệp, Quận 12, Hồ Chí Minh, Việt Nam</b><br />
-                        <span className='font-14'>Trạng thái</span><br />
-                        <span className='font-14'>Thời gian mở cửa</span><br />
-                        <span className='font-14'>Số lượng sân</span>
-                    </div>
-                    <div className='me-3 d-flex align-items-center'>
-                        <div className='btn-group'>
-                            <Link href={'/owner/sport-manager/view-list-sports/1'} className='btn btn-secondary btn-hv'><i className="bi bi-eye"></i></Link>
-                            <Link href={'/owner/sport-manager/edit-sport/2'} className='btn btn-warning btn-hv'><i className="bi bi-pencil-square"></i></Link>
+            {sportFields && sportFields.map((sf, index: number) => (
+                <div key={sf.id || index}>
+                    <>
+                        <div className='card p-2'>
+                            <div className='d-flex justify-content-between'>
+                                <div>
+                                    <b className='mx-3'>{index + 1}</b>
+                                    <img src={`http://localhost:8080/images/images_sportfield/${sf.image}`}
+                                        className='me-3' style={{ width: '300px' }} alt="cc" />
+                                </div>
+                                <div className='me-auto mt-3'>
+                                    <b>{sf.name}</b><br />
+                                    <b className='font-14'>{sf.address}</b><br />
+                                    <span className='font-14'>Trạng thái: {sf.status}</span><br />
+                                    <span className='font-14'>Thời gian mở cửa: {sf.opening} - {sf.closing}</span><br />
+                                    <span className='font-14'>Số lượng sân: {sf.quantity}</span>
+                                </div>
+                                <div className='me-3 d-flex align-items-center'>
+                                    <div className='btn-group'>
+                                        <Link href={`/owner/sport-manager/view-list-sports/${sf.sportFieldId}`} className='btn btn-secondary btn-hv'><i className="bi bi-eye"></i></Link>
+                                        <Link href={'/owner/sport-manager/edit-sport/2'} className='btn btn-warning btn-hv'><i className="bi bi-pencil-square"></i></Link>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        <br />
+                    </>
                 </div>
-            </div>
-            <br />
+
+            ))}
             <div className="card">
                 <Link href={'/owner?check=withdraw'} className='btn btn-danger'><i className="bi bi-plus-circle me-2"></i>Thêm khu vực</Link>
             </div>
