1a34c85fb080802f41118ce72584cd6728e3cb08
DieuMy25 <131366686+DieuMy25@users.noreply.github.com>
2024-10-23 15:38:05 +0700

    SportFieldManager

--- MapogoSport-BE/src/main/java/mapogo/service/SportFieldService.java
+++ MapogoSport-BE/src/main/java/mapogo/service/SportFieldService.java
@@ -8,7 +8,11 @@ public interface SportFieldService {
 	List<SportField> findAll();
 	
 	SportField findBySportFieldId(Integer Id);
+	
 	//Mỵ sửa lỗi chính tả
 	List<SportField> findSportFieldByOwner(Integer id);
 	
+	//Của Mỵ 
+	SportField create(SportField sportField);
+	//
 }
--- MapogoSport-FE/src/app/owner/sport-manager/edit-sport/[id]/page.tsx
+++ MapogoSport-FE/src/app/owner/sport-manager/edit-sport/[id]/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
+import Time from 'react-datepicker/dist/time';
 
 const EditSport = () => {
     const [showAddAddress, setShowAddAddress] = useState<boolean>(false)
@@ -30,50 +31,62 @@ const EditSport = () => {
     const [name, setName] = useState<string>('');
     const [description, setDescription] = useState<string>('');
     const [address, setAddress] = useState<string>('');
+    const [status, setStatus] = useState<string>('');
     const [category, setCategory] = useState<number | undefined>(undefined);
+    const [opening, setOpening] = useState<String>('');
+    const [closing, setClosing] = useState<String>('');
     useEffect(() => {
         if (sportField) {
             setName(sportField.name);
             setDescription(sportField.decription);
             setAddress(sportField.address);
             setCategory(sportField.categoriesField?.categoriesFieldId); // Access categoriesFieldId correctly
-            console.log(typeof (sportField.categoriesField?.categoriesFieldId))
-            console.log(typeof (category))
-            console.log(category)
-            console.log(sportField.categoriesField?.categoriesFieldId)
-
+            setStatus(sportField.status)
+            setOpening(sportField.opening)
+            setClosing(sportField.closing)
 
         }
-
     }, [sportField]);
+
     return (
         <>
-            <h3 className="text-center text-danger fw-bold" style={{ fontSize: '20px' }}>CHỈNH SỬA THÔNG TIN SÂN "TÊN SÂN"</h3>
+            <h3 className="text-center text-danger fw-bold" style={{ fontSize: '20px' }}>CHỈNH SỬA THÔNG TIN SÂN "{name}"</h3>
             <div className="row">
                 <div className="col-8">
                     <div className="row">
-                        <div className="col-md-8 form-floating mb-3">
+                        <div className="col form-floating mb-3">
                             <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com"
                                 value={name || ''}
                                 onChange={(e) => setName(e.target.value)} // Cập nhật state khi người dùng nhập
                             />
                             <label className='ms-2' htmlFor="floatingInput">Tên sân</label>
-
                         </div>
-                        <div className=" col-md-4 form-floating mb-3">
-                            <input type="time" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                            <label className='ms-2' htmlFor="floatingInput">{sportField.oppening}</label>
+                    </div>
+                    <div className="row">
+                        <div className=" col-md-6 form-floating mb-3">
+                            <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com"
+                                value={opening || ''}
+                                onChange={(e) => setOpening(e.target.value)}
+                            />
+                            <label className='ms-2' htmlFor="floatingInput">Giờ mở cửa</label>
+                        </div>
+                        <div className=" col-md-6 form-floating mb-3">
+                            <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com"
+                                value={closing || ''}
+                                onChange={(e) => setClosing(e.target.value)}
+                            />
+                            <label className='ms-2' htmlFor="floatingInput">Giờ đóng cửa</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 form-floating">
                             <select className="form-select" id="floatingSelect" aria-label="Floating label select example"
-                                value={category !== undefined ? category.toString() : ''} // Convert to string for the select value
+                                value={category ?? ''}
                                 onChange={(e) => setCategory(Number(e.target.value))}
                             >
-                                <option>Chọn loại sân</option>
+                                <option value="">Chọn loại sân</option>
                                 {categories.map(category => (
-                                    <option key={category.id} value={category.id?.toString() || ''}>
+                                    <option key={category.categoriesFieldId} value={category.categoriesFieldId}>
                                         {category.name}
                                     </option>
                                 ))}
@@ -81,17 +94,20 @@ const EditSport = () => {
                             <label className='ms-2' htmlFor="floatingSelect">Loại sân</label>
                         </div>
                         <div className="col-md-6 form-floating">
-                            <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
-                                <option selected>Chọn trạng thái</option>
-                                <option value="1">Hoạt động</option>
-                                <option value="2">Tạm đóng</option>
-                                <option value="3">Sửa chữa</option>
+                            <select className="form-select" id="floatingSelect" aria-label="Floating label select example"
+                                value={status}
+                                onChange={(e) => setStatus(e.target.value)}>
+                                <option >Chọn trạng thái</option>
+                                <option value="Hoạt động">Hoạt động</option>
+                                <option value="Tạm đóng">Tạm đóng</option>
+                                <option value="Sửa chữa">Sửa chữa</option>
                             </select>
                             <label className='ms-2' htmlFor="floatingSelect">Trạng thái</label>
                         </div>
                         <div className="col form-floating mt-3">
                             <input type="text" className="form-control" id="floatingInput"
-                                value={sportField.address || ''} />
+                                value={address || ''}
+                                onChange={(e) => setAddress(e.target.value)} />
                             <label htmlFor="floatingInput" className='ms-2'>Địa chỉ</label>
                         </div>
                     </div>
@@ -102,7 +118,7 @@ const EditSport = () => {
                         <label htmlFor="floatingTextarea2">Mô tả</label>
                     </div>
                     <Button style={{ width: "100%" }} variant='danger' className='mt-3' onClick={() => setShowAddAddress(true)}>
-                        <i className="bi bi-plus-circle"></i> Đổi địa chỉ
+                        <i className="bi bi-plus-circle"></i> Cập nhật
                     </Button>
                 </div>
                 <div className="col-4">
@@ -112,7 +128,7 @@ const EditSport = () => {
                                 <img src="/images/logo.png" style={{ width: '100%' }} alt="" />
                                 <input type="file" name="avatar" id="imageUpload"
                                     accept="image/jpeg, image/png" style={{ display: 'none' }} />
-                                <label htmlFor="imageUpload" className="btn btn-link"> Cập nhật </label>
+                                <label htmlFor="imageUpload" className="btn btn-link"> Sửa ảnh </label>
                             </div>
                             <div className="avatar-preview">
                             </div>
@@ -120,7 +136,6 @@ const EditSport = () => {
                     </div>
                 </div>
             </div>
-            <ModalAddAddress showAddAddress={showAddAddress} setShowAddAddress={setShowAddAddress} />
 
             <Link href={'/owner/sport-manager'} className='btn btn-success btn-hv mt-3'>Quay lại</Link>
         </>
