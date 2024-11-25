0aea0a6806aa16e2201428d26feaf1e98dfa2cb4
Phi Hung <phihung.devka@gmail.com>
2024-11-25 12:06:44 +0700

    delete api voucher unnecessary

--- MapogoSport-BE/src/main/java/mapogo/service/impl/VoucherServiceImpl.java
+++ MapogoSport-BE/src/main/java/mapogo/service/impl/VoucherServiceImpl.java
@@ -77,21 +77,6 @@ public class VoucherServiceImpl implements VoucherService{
 	public Voucher findById(int id) {
 		return dao.findById(id).get();
 	}
-
-	@Override
-	public List<Voucher> findByUserName(String username) {
-		List<UserVoucher> userVouchers = userVoucherService.findByUsername(username);
-		List<UserVoucher> unusedVouchers = userVouchers.stream()
-			    .filter(voucher -> "Unused".equals(voucher.getStatus())) 
-			    .collect(Collectors.toList());
-		List<Voucher> vouchers =  new ArrayList<>();
-		for (UserVoucher userVoucher : unusedVouchers) {
-			Voucher voucher = userVoucher.getVoucher();
-		    if (voucher.getEndDate().isAfter(LocalDateTime.now())) { 
-		        vouchers.add(voucher); 
-		    }		}
-		return vouchers;
-	}
 	//đến đây
 
 	@Override
